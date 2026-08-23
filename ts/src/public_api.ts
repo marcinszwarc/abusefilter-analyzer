e8afce066e21b5b86441087c0899a66646eeb37a
Marcin Szwarc <marcinszwarc@hotmail.com>
2026-08-23 12:57:41 +0200

    Fix MassCheck tick/cross in light mode

--- ts/src/public_api.ts
+++ ts/src/public_api.ts
@@ -123,7 +123,7 @@ mw.hook('userjs.abuseFilter').fire(_abuseFilter);
 
 // TODO: put this to the GUI package
 mw.util.addCSS(`
-.afa-tree-container, .afa-value {
+.afa-tree-container, .afa-value, .afa-masscheck-result {
     --afa-color-value-keyword: blue;
     --afa-color-value-string: brown;
     --afa-color-value-number: darkgreen;
